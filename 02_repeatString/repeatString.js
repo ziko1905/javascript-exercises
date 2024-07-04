0f4e6e2817595e00e080115ec5d881f90353cba7
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-04 17:59:20 +0200

    Have first three excercises finshed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    let res = "";
+    for (let i = 0; i < num; i++) {
+        res += string
+    }
+    return res
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        outputString += inputString[i]
+    }
+    return outputString
 };
 
 // Do not edit below this line
