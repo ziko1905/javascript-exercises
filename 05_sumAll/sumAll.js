9ad5562e36afb5c01d049ef6a3add2ac4b1d4e8d
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-05 11:33:27 +0200

    Have all finished tasks up until 5th

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number" ||a < 0 || b < 0) return "ERROR"
+    return ((a + b) / 2) * (Math.max(b, a) - Math.min(a, b) + 1)
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
